2306c3cfa604830ae7f7eb0b478d7fe6e12e858d
dma1979 <DemidOvich3>
2019-06-22 14:18:36 +0300

    Reactive Spring Demo project(rsocket,mongodb)

---
+++ reactive-daily-qa/src/main/java/by/dma1979/reactivedailyqa/ReactiveDailyQaApplication.java
@@ -0,0 +1,66 @@
+package by.dma1979.reactivedailyqa;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Consumer;
+
+@SpringBootApplication
+public class ReactiveDailyQaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveDailyQaApplication.class, args);
+    }
+
+}
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+class DataInitializer {
+    private final BranchRepository repository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void go() {
+
+        var branchFlux = Flux
+                .just("2019.1", "2019.2", "2019.3", "2019.4") // Flux<String>
+                .map(branch -> new Branch(null, branch)) // Flux<Branch>
+                .flatMap(repository::save); //Flux<Branch>
+
+        this.repository
+                .deleteAll()
+                .thenMany(branchFlux)
+                .thenMany(repository.findAll())
+                .subscribe(log::info);
+    }
+
+}
+
+
+interface BranchRepository extends ReactiveCrudRepository<Branch, String> {
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+class Branch {
+    @Id
+    private String id;
+
+    private String name;
+
+}
---
+++ reactive-daily-qa/src/test/java/by/dma1979/reactivedailyqa/ReactiveDailyQaApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dma1979.reactivedailyqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDailyQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
