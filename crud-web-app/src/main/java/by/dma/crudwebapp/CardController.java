e5fe0e07c2c69f27d8fc71cd2ad4e1fd2df6279f
Dzmitry Maridau <vinmaster@tut.by>
2019-07-11 01:18:47 +0300

    CRUD Web app: controller#createCard -call business service to create card - prepare correct response entity

--- crud-web-app/src/main/java/by/dma/crudwebapp/CardController.java
+++ crud-web-app/src/main/java/by/dma/crudwebapp/CardController.java
@@ -1,8 +1,18 @@
 package by.dma.crudwebapp;
 
+import by.dma.crudwebapp.dto.CardRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/cards")
@@ -10,4 +20,15 @@ public class CardController {
 
     @Autowired
     private CardService service;
+
+    @PostMapping
+    ResponseEntity<Void> createCard(@Valid @RequestBody CardRequestDTO request, UriComponentsBuilder builder) {
+        Long cardId = service.createCard(request);
+
+        URI uri = builder.path("/api/cards/{id}").buildAndExpand(cardId).toUri();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(uri);
+
+        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
+    }
 }
