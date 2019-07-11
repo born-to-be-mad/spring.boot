1b66b8a34971b7542b29f3d3ad9660c124213ee4
Dzmitry Marudau <d.marudau@sam-solutions.com>
2019-07-11 16:59:54 +0300

    Card WEB App: use concrete port and re-structure the project.

--- crud-web-app/src/main/java/by/dma/crudwebapp/controller/CardRepository.java
+++ crud-web-app/src/main/java/by/dma/crudwebapp/controller/CardRepository.java
@@ -4,7 +4,8 @@ import by.dma.crudwebapp.controller.model.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * This JPA Repository have access to our data in database by using JPA Entity Manager behind the scene to access the data .
+ * This JPA Repository have access to our data in database by using JPA Entity Manager
+ * behind the scene to access the data .
  *
  * @author dzmitry.marudau
  * @since 2019.4
--- crud-web-app/src/main/java/by/dma/crudwebapp/controller/dto/CardRequestDTO.java
+++ crud-web-app/src/main/java/by/dma/crudwebapp/controller/dto/CardRequestDTO.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
