5470b3c63b2539c2fce61d8a765576e0fc0f5d4c
Dzmitry Maridau <vinmaster@tut.by>
2019-07-12 11:00:11 +0300

    IT test for CardController

---
+++ crud-web-app/src/test/java/by/dma/crudwebapp/CardControllerIT.java
@@ -0,0 +1,57 @@
+package by.dma.crudwebapp;
+
+import by.dma.crudwebapp.controller.CrudWebAppApplication;
+import com.fasterxml.jackson.databind.JsonNode;
+import javafx.application.Application;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = CrudWebAppApplication.class)
+public class CardControllerIT {
+
+    public static final String API_CARDS = "/api/cards/";
+    @LocalServerPort
+    int randomServerPort;
+
+    private TestRestTemplate testRestTemplate;
+
+    @BeforeEach
+    public void setUp() {
+        this.testRestTemplate = new TestRestTemplate();
+    }
+
+    @Test
+    public void deletingKnownCardShouldNotBeFoundAfterDeletion() {
+        long entityId = 1;
+        String baseUrl = getBaseUrl();
+
+        ResponseEntity<JsonNode> initialResponse =
+                testRestTemplate.getForEntity(baseUrl + API_CARDS + entityId, JsonNode.class);
+
+        assertThat(initialResponse.getStatusCode(), is(HttpStatus.OK));
+
+        this.testRestTemplate.delete(baseUrl + API_CARDS + entityId);
+
+        ResponseEntity<JsonNode> updatedResponse =
+                testRestTemplate.getForEntity(baseUrl + API_CARDS + entityId, JsonNode.class);
+
+        assertThat(updatedResponse.getStatusCode(), is(HttpStatus.NOT_FOUND));
+    }
+
+    private String getBaseUrl() {
+        return "http://localhost:" + randomServerPort;
+    }
+}
--- crud-web-app/src/test/java/by/dma/crudwebapp/CrudWebAppApplicationTests.java
+++ crud-web-app/src/test/java/by/dma/crudwebapp/CrudWebAppApplicationTests.java
@@ -1,4 +1,4 @@
-package by.dma.crudwebapp.controller;
+package by.dma.crudwebapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
