d558df2f26f06f479f255440b6ee478a8f93d4ef
Dzmitry Maridau <vinmaster@tut.by>
2019-10-30 01:37:26 +0300

    Spring Boot Basics: check console output via OutputCaptureExtension

--- spring-boot-recipies/src/test/java/by/dma1979/recipes/calculator/CalculatorIT.java
+++ spring-boot-recipies/src/test/java/by/dma1979/recipes/calculator/CalculatorIT.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith({SpringExtension.class, OutputCaptureExtension.class})
 @SpringBootTest(classes = SpringBootRecipesApplication.class)
 public class CalculatorIT {
 
@@ -20,6 +24,12 @@ public class CalculatorIT {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             calculator.calculate(12, 13, '/');
         });
+    }
+
+    @Test
+    public void doingMultiplicationShouldSucceed(CapturedOutput output) {
+        calculator.calculate(12,13, '*');
+        assertThat(output.getOut()).contains("12 * 13 = 156");
     }
 
 }
