e0747520a16bd07f233407e56cd439255a5cac6a
Dzmitry Maridau <vinmaster@tut.by>
2019-10-31 00:59:19 +0300

    Spring Boot Basics: configure logging - exclude logback , add log4j2

--- spring-boot-recipies/src/main/java/by/dma1979/recipes/SpringBootRecipesApplication.java
+++ spring-boot-recipies/src/main/java/by/dma1979/recipes/SpringBootRecipesApplication.java
@@ -1,6 +1,8 @@
 package by.dma1979.recipes;
 
 import by.dma1979.recipes.calculator.Calculator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,8 +15,12 @@ import java.util.Arrays;
 @SpringBootApplication
 public class SpringBootRecipesApplication {
 
+    private static final Logger LOG = LogManager.getLogger(SpringBootRecipesApplication.class);
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SpringBootRecipesApplication.class, args);
+        LOG.info("Hello from Log4j 2 - ConfigurableApplicationContext : {}", () -> context);
+
         System.out.println("###############   BOOTING........");
         System.out.printf("# There are %d bean definitions:%n", context.getBeanDefinitionCount());
         String[] beanNames = context.getBeanDefinitionNames();
@@ -23,6 +29,8 @@ public class SpringBootRecipesApplication {
         System.out.println("############################################");
         System.out.println("###############   THE END    ###############");
         System.out.println("############################################");
+
+        LOG.info("###############   THE END    ###############");
     }
 
     @Bean
