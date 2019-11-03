ea23cb175c123cefc19d3a6778cf1cdb8a0a4695
Dzmitry Maridau <vinmaster@tut.by>
2019-11-04 01:04:08 +0300

    Spring MVC: Internationalizing(i18) - SessionLocaleResolver

--- spring-boot-recipies/src/main/java/by/dma1979/SpringBootRecipesApplication.java
+++ spring-boot-recipies/src/main/java/by/dma1979/SpringBootRecipesApplication.java
@@ -12,8 +12,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringBootRecipesApplication {
@@ -63,4 +66,10 @@ public class SpringBootRecipesApplication {
         return new CustomizedErrorAttributes();
     }
 
+    @Bean
+    public LocaleResolver localeResolver () {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("ru"));
+        return localeResolver;
+    }
 }
