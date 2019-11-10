958172a9b6fce5e1bdfc9b9f168452efcbfa745a
Dzmitry Maridau <vinmaster@tut.by>
2019-11-11 01:26:53 +0300

    Spring WebFlux: MOCK Web Environment + @AutoConfigureWebTestClient

---
+++ spring-boot-recipies/src/test/java/by/dma1979/order/web/OrderControllerIT.java
@@ -0,0 +1,50 @@
+package by.dma1979.order.web;
+
+import by.dma1979.order.Order;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.math.BigDecimal;
+
+/**
+ * @author : Dzmitry Marudau
+ * @created at : 01:05
+ * @since : 2019.11
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+/*@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureWebTestClient*/
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class OrderControllerIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void listOrders() {
+        webTestClient.get().uri("/orders")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Order.class).hasSize(25);
+    }
+
+    @Test
+    public void addAndGetOrder() {
+        var order = new Order("test1", BigDecimal.valueOf(1234.56));
+        webTestClient.post().uri("/orders").syncBody(order)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Order.class).isEqualTo(order);
+        webTestClient.get().uri("/orders/{id}", order.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Order.class).isEqualTo(order);
+
+    }
+}
