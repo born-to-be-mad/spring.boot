dedb87660d63a0a6450902f7812f58ea3d2a9e60
Dzmitry Maridau <vinmaster@tut.by>
2019-11-11 00:41:51 +0300

    Avoid a*-imports.

--- spring-boot-recipies/src/main/java/by/dma1979/order/web/OrderController.java
+++ spring-boot-recipies/src/main/java/by/dma1979/order/web/OrderController.java
@@ -2,7 +2,12 @@ package by.dma1979.order.web;
 
 import by.dma1979.order.Order;
 import by.dma1979.order.OrderService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
