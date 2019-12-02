b250fc9253b085d15f42d0594ac6084e1e1be3d6
Dzmitry Maridau <vinmaster@tut.by>
2019-12-03 01:02:04 +0300

    Spring Boot - Data access: TestEntityManager offers some convenience  methods to easily store and find data for testing.

--- spring-boot-recipies/src/main/java/by/dma1979/jpa/entity/Customer.java
+++ spring-boot-recipies/src/main/java/by/dma1979/jpa/entity/Customer.java
@@ -1,25 +1,30 @@
 package by.dma1979.jpa.entity;
 
-import lombok.Data;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Customer {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-	@Column(nullable = false)
-	private String name;
+    @Column(nullable = false)
+    private String name;
 
-	@Column(nullable = false)
-	private String email;
+    @Column(nullable = false)
+    private String email;
 
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 }
---
+++ spring-boot-recipies/src/test/java/by/dma1979/jpa/spring/CustomerRepositoryTest.java
@@ -0,0 +1,54 @@
+package by.dma1979.jpa.spring;
+
+import by.dma1979.jpa.entity.Customer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author : Dzmitry Marudau
+ * @created at : 00:41
+ * @since : 2019.12
+ **/
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@TestPropertySource(properties = "spring.flyway.enabled=false")
+public class CustomerRepositoryTest {
+
+  @Autowired
+  private CustomerRepository repository;
+
+  @Autowired
+  private TestEntityManager testEntityManager;
+
+  @Test
+  public void insertNewCustomer() {
+    assertThat(repository.findAll()).isEmpty();
+
+    Customer customer = repository.save(new Customer("Dzmitry Alexandrov", "support@tut.by"));
+
+    assertThat(customer.getId()).isGreaterThan(-1L);
+    assertThat(customer.getName()).isEqualTo("Dzmitry Alexandrov");
+    assertThat(customer.getEmail()).isEqualTo("support@tut.by");
+
+    assertThat(repository.findById(customer.getId())).hasValue(customer);
+  }
+
+  @Test
+  public void findAllCustomers() {
+    assertThat(repository.findAll()).isEmpty();
+
+    testEntityManager.persistAndFlush(new Customer("Dzmitry Alexandrov", "support@tut.by"));
+    testEntityManager.persistAndFlush(new Customer("Iva Alexandrov", "support@tut.by"));
+
+    assertThat(repository.findAll()).hasSize(2);
+  }
+
+
+}
