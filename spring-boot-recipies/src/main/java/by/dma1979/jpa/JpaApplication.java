f0a54a034192a4f5a6544b97d6f52f649acbc0e8
Dzmitry Maridau <vinmaster@tut.by>
2019-12-05 01:18:12 +0300

    Spring Boot - Data access: Native Hibernate repository(using the SessionFactory)

--- spring-boot-recipies/src/main/java/by/dma1979/jpa/JpaApplication.java
+++ spring-boot-recipies/src/main/java/by/dma1979/jpa/JpaApplication.java
@@ -1,6 +1,7 @@
 package by.dma1979.jpa;
 
 import by.dma1979.jpa.spring.SpringDataJpaCustomerRepository;
+import org.hibernate.cfg.AvailableSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
@@ -8,17 +9,41 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @SpringBootApplication
 @EntityScan({
         "by.dma1979.entity",
-        "by.dma1979.jpa.entity" })
+        "by.dma1979.jpa.entity"})
 public class JpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
     }
+
+    @Bean(name = "entityManagerFactory")
+    LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        Properties properties = new Properties();
+        properties.setProperty(AvailableSettings.DIALECT, "org.hibernate.dialect.PostgreSQL95Dialect");
+        properties.setProperty(AvailableSettings.NON_CONTEXTUAL_LOB_CREATION, "true");
+        //properties.setProperty(AvailableSettings.CONNECTION_PROVIDER_DISABLES_AUTOCOMMIT, "true");
+
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setPackagesToScan("by.dma1979.jpa", "by.dma1979.jpa.entity");
+        sessionFactoryBean.setHibernateProperties(properties);
+        return sessionFactoryBean;
+    }
+
+//  @Bean
+//  public PlatformTransactionManager transactionManager(SessionFactory sf) {
+//	  return new HibernateTransactionManager(sf);
+//  }
 }
 
 @Component
@@ -32,12 +57,16 @@ class CustomerLister implements ApplicationRunner {
 
     private final ICustomerRepository plainHibernateCustomerRepository;
 
+    private final ICustomerRepository nativeHibernateCustomerRepository;
+
     CustomerLister(PlainJpaCustomerRepository plainRepository,
                    SpringDataJpaCustomerRepository springDataJpaCustomerRepository,
-                   PlainHibernateCustomerRepository plainHibernateCustomerRepository) {
+                   PlainHibernateCustomerRepository plainHibernateCustomerRepository,
+                   NativeHibernateCustomerRepository nativeHibernateCustomerRepository) {
         this.plainRepository = plainRepository;
         this.springDataJpaCustomerRepository = springDataJpaCustomerRepository;
         this.plainHibernateCustomerRepository = plainHibernateCustomerRepository;
+        this.nativeHibernateCustomerRepository = nativeHibernateCustomerRepository;
     }
 
     @Override
@@ -50,5 +79,8 @@ class CustomerLister implements ApplicationRunner {
 
         logger.debug("### Plain Hibernate repository ###");
         plainHibernateCustomerRepository.findAll().forEach(customer -> logger.info("{}", customer));
+
+        logger.debug("### Plain NativeHibernate repository ###");
+        nativeHibernateCustomerRepository.findAll().forEach(customer -> logger.info("{}", customer));
     }
 }
