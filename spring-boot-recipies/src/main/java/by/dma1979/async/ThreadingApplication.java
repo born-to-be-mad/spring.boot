a8164e50e8a27d280cd9baabfb2174e2b8ad1589
Dzmitry Maridau <vinmaster@tut.by>
2019-12-24 01:43:50 +0300

    Spring Boot - threading application

--- spring-boot-recipies/src/main/java/by/dma1979/async/ThreadingApplication.java
+++ spring-boot-recipies/src/main/java/by/dma1979/async/ThreadingApplication.java
@@ -1,17 +1,13 @@
 package by.dma1979.async;
 
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
@@ -23,13 +19,13 @@ import java.io.IOException;
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@Profile("noserver")
 public class ThreadingApplication {
     public static void main(String[] args) throws IOException {
         new SpringApplicationBuilder(ThreadingApplication.class)
                 .web(WebApplicationType.NONE)
                 .build()
                 .run(args);
-
         System.out.println("Press [ENTER] to quit:");
         System.in.read();
     }
