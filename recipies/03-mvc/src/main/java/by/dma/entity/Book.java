4999aa5b7dfacf273ad5595fdf9415d27fa67eae
Dzmitry Marudau <dmarudau@cpaglobal.com>
2020-05-24 20:00:57 +0300

    Add Book entity, service and rest endpoint

---
+++ recipies/03-mvc/src/main/java/by/dma/entity/Book.java
@@ -0,0 +1,36 @@
+package by.dma.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A book is defined by its ISBN number; it has a title and 1 or more authors.
+ *
+ * @author : Dzmitry Marudau
+ * @created at : 00:32
+ * @since : 2020.05
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Book {
+    private String isbn;
+    private String title;
+    private List<String> authors = new ArrayList<>();
+
+    public Book(String isbn, String title, String... authors) {
+        this.isbn = isbn;
+        this.title = title;
+        this.authors.addAll(Arrays.asList(authors));
+    }
+
+}
---
+++ recipies/03-mvc/src/main/java/by/dma/service/impl/InMemoryBookService.java
@@ -0,0 +1,49 @@
+package by.dma.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+import by.dma.entity.Book;
+import by.dma.service.BookService;
+
+/**
+ * The service has been annotated with @Service so that Spring Boot will detect it and
+ * create an instance of it.
+ *
+ * @author : Dzmitry Marudau
+ * @created at : 00:36
+ * @since : 2020.05
+ **/
+@Service
+class InMemoryBookService implements BookService {
+    private final Map<String, Book> books = new ConcurrentHashMap<>();
+
+    @Override
+    public Iterable<Book> findAll() {
+        return books.values();
+    }
+
+    @Override
+    public Book create(Book book) {
+        books.put(book.getIsbn(), book);
+        return book;
+    }
+
+    @Override
+    public Optional<Book> find(String isbn) {
+        return Optional.ofNullable(books.get(isbn));
+    }
+
+    @Override
+    public Optional<Book> update(String isbn, Book book) {
+        return Optional.ofNullable(books.computeIfPresent(isbn, (k, v) -> book));
+    }
+
+    @Override
+    public Optional<Book> delete(String isbn) {
+        return Optional.ofNullable(books.remove(isbn));
+    }
+}
