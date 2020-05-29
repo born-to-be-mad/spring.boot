da90112aed8a6383447d566959d0d2d70a2e54ec
Dzmitry Marudau <vinmaster@tut.by>
2020-05-30 01:01:02 +0300

    Spring MVC asynch: tests for callable and computable future.

--- recipies/04-mvc-asynch/src/main/java/by/dma/MvcAsynchDemoApplication.java
+++ recipies/04-mvc-asynch/src/main/java/by/dma/MvcAsynchDemoApplication.java
@@ -1,18 +1,9 @@
 package by.dma;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-
-import java.util.Locale;
 
 /**
  * @author : Dzmitry Marudau
