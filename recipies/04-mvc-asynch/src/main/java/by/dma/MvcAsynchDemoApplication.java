cc4acb9829853eb8b410c75c4244a2f46face98c
Dzmitry Marudau <vinmaster@tut.by>
2020-05-30 01:03:26 +0300

    Spring MVC asynch: configure async processing

--- recipies/04-mvc-asynch/src/main/java/by/dma/MvcAsynchDemoApplication.java
+++ recipies/04-mvc-asynch/src/main/java/by/dma/MvcAsynchDemoApplication.java
@@ -3,6 +3,9 @@ package by.dma;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -16,6 +19,18 @@ public class MvcAsynchDemoApplication implements WebMvcConfigurer {
   public static void main(String[] args) {
     ConfigurableApplicationContext context =
             SpringApplication.run(MvcAsynchDemoApplication.class, args);
+  }
+
+  @Override
+  public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+    configurer.setTaskExecutor(mvcTaskExecutor());
+  }
+
+  @Bean
+  public ThreadPoolTaskExecutor mvcTaskExecutor() {
+    ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+    taskExecutor.setThreadNamePrefix("mvc-task-");
+    return taskExecutor;
   }
 
 }
