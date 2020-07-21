db8c78041948c85172062c67fa8d69c4d0a17a39
Dzmitry Marudau <vinmaster@tut.by>
2020-07-22 01:06:52 +0300

    Split table listeners

--- recipies/08-data-access/src/main/java/by/dma/SpringDataAccessApplication.java
+++ recipies/08-data-access/src/main/java/by/dma/SpringDataAccessApplication.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Component;
 
 import by.dma.jdbc.repository.CustomerRepository;
 
+import static by.dma.SpringDataAccessApplication.SELECT_SQL;
+
 /**
  * @author : Dzmitry Marudau
  * @created at : 00:24
@@ -21,6 +23,7 @@ import by.dma.jdbc.repository.CustomerRepository;
  **/
 @SpringBootApplication
 public class SpringDataAccessApplication {
+  public static final String SELECT_SQL = "SELECT id, name, email FROM customer";
 
   public static void main(String[] args) {
     ConfigurableApplicationContext context = SpringApplication.run(
@@ -29,56 +32,19 @@ public class SpringDataAccessApplication {
 }
 
 @Component
-class TableLister implements ApplicationRunner {
+class JdbcRepositoryTableLister implements ApplicationRunner {
 
   private final Logger logger = LoggerFactory.getLogger(getClass());
-  private final DataSource dataSource;
-  private final JdbcTemplate jdbc;
+
   private final CustomerRepository customerRepository;
 
-  TableLister(
-      DataSource dataSource, JdbcTemplate jdbc,
-      CustomerRepository customerRepository) {
-    this.dataSource = dataSource;
-    this.jdbc = jdbc;
+  JdbcRepositoryTableLister(CustomerRepository customerRepository) {
     this.customerRepository = customerRepository;
   }
 
   @Override
-  public void run(ApplicationArguments args) throws Exception {
-    try (
-        var con = dataSource.getConnection();
-        var rs = con.getMetaData().getTables(null,
-                                             null,
-                                             "%",
-                                             null)) {
-      while (rs.next()) {
-        logger.info("{}", rs.getString(3));
-      }
-    }
-
-    System.out.println("###### DB content via DataSource ######");
-    var query = "SELECT id, name, email FROM customer";
-    try (
-        var con = dataSource.getConnection();
-        var stmt = con.createStatement();
-        var rs = stmt.executeQuery(query)) {
-      while (rs.next()) {
-        logger.info("Customer [id={}, name={}, email={}]",
-                    rs.getLong(1), rs.getString(2),
-                    rs.getString(3));
-      }
-    }
-
-    System.out.println("###### DB content via JdbcTemplate ######");
-    jdbc.query(query, rs -> {
-      logger.info("Customer [id={}, name={}, email={}]",
-                  rs.getLong(1),
-                  rs.getString(2),
-                  rs.getString(3));
-    });
-
-    System.out.println("###### DB content via Jdbc repository ######");
+  public void run(ApplicationArguments args) {
+    System.out.println("###### DB content via Jdbc repository -> START ######");
     customerRepository.findAll()
                       .forEach(customer -> logger.info("{}", customer));
 
@@ -86,21 +52,52 @@ class TableLister implements ApplicationRunner {
 }
 
 @Component
-class CustomerLister implements ApplicationRunner {
+class JdbcTemplateTableLister implements ApplicationRunner {
 
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+  private final JdbcTemplate jdbc;
+
+  JdbcTemplateTableLister(JdbcTemplate jdbc) {
+    this.jdbc = jdbc;
+  }
+
+  @Override
+  public void run(ApplicationArguments args) {
+    System.out.println("###### DB content via JdbcTemplate -> START ######");
+    jdbc.query(SELECT_SQL, rs -> {
+      logger.info("Customer [id={}, name={}, email={}]",
+                  rs.getLong(1),
+                  rs.getString(2),
+                  rs.getString(3));
+    });
+  }
+}
+
+@Component
+class CustomerDataSourceLister implements ApplicationRunner {
   private final Logger logger = LoggerFactory.getLogger(getClass());
   private final DataSource dataSource;
 
-  CustomerLister(DataSource dataSource) {
+  CustomerDataSourceLister(DataSource dataSource) {
     this.dataSource = dataSource;
   }
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
+    System.out.println("###### DB content via DATASOURCE -> METADATA ######");
+    try (
+        var con = dataSource.getConnection();
+        var rs = con.getMetaData()
+                    .getTables(null, null, "%", null)) {
+      while (rs.next()) {
+        logger.info("{}", rs.getString(3));
+      }
+    }
+    System.out.println("###### DB content via DATASOURCE -> START ######");
     try (
         var con = dataSource.getConnection();
         var stmt = con.createStatement();
-        var rs = stmt.executeQuery("SELECT id, name, email FROM customer")
+        var rs = stmt.executeQuery(SELECT_SQL)
     ) {
       while (rs.next()) {
         logger.info("Customer [id={}, name={}, email={}]",
@@ -109,5 +106,6 @@ class CustomerLister implements ApplicationRunner {
                     rs.getString(3));
       }
     }
+    System.out.println("###### DB content via DATASOURCE -> END ######");
   }
 }
