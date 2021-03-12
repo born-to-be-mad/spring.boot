2bddd106ccdc64830959fe36bd9f5f3b53f12ed3
Dzmitry Marudau <vinmaster@tut.by>
2021-03-12 17:43:58 +0300

    Spring Cloud: add 'spring-cloud config' module

--- spring-cloud/quest-services/src/test/java/by/dma/guest/GuestServicesApplicationTests.java
+++ spring-cloud/quest-services/src/test/java/by/dma/guest/GuestServicesApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QuestServicesApplicationTests {
+class GuestServicesApplicationTests {
 
   @Test
   void contextLoads() {
