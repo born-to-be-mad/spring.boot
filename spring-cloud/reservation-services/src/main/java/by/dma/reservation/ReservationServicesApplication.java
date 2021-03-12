c6be0ed9f58a185254c62b254e6ee2d4562e6ed1
Dzmitry Marudau <vinmaster@tut.by>
2021-03-12 18:04:18 +0300

    Spring Cloud: register all API microservices in Eureka

--- spring-cloud/reservation-services/src/main/java/by/dma/reservation/ReservationServicesApplication.java
+++ spring-cloud/reservation-services/src/main/java/by/dma/reservation/ReservationServicesApplication.java
@@ -2,8 +2,10 @@ package by.dma.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReservationServicesApplication {
 
   public static void main(String[] args) {
