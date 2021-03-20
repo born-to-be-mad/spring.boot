3093c3c6075fa06a1cc0adbcb0ba209f414c4fdf
Dzmitry Marudau <vinmaster@tut.by>
2021-03-21 01:25:10 +0300

    Spring Boot 2 Essentials: The basics

---
+++ boot-two-essential/basics/src/test/java/by/dma/basics/BasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dma.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
