f53d1403f4d0d40e4f8f1a2ec25ddf0d704e63c1
Dzmitry Marudau <vinmaster@tut.by>
2022-10-06 01:02:28 +0300

    Spring AOP: process result pointcut configuration

--- spring-aop/src/main/java/by/dma/aop/aspect/AspectConfig.java
+++ spring-aop/src/main/java/by/dma/aop/aspect/AspectConfig.java
@@ -40,10 +40,23 @@ public class AspectConfig {
 
   @Bean
   @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-  public PointcutAdvisor publishResultPointcutAdvisor(
-          ApplicationEventPublisher eventPublisher) {
-
+  public PointcutAdvisor publishResultPointcutAdvisor(ApplicationEventPublisher eventPublisher) {
     return new DefaultPointcutAdvisor(new PublishResultMethodPointcut(),
             new PublishResultAfterReturnAdvice(eventPublisher));
   }
+
+  //    @Bean
+  //    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+  //    public DefaultBeanFactoryPointcutAdvisor publishResultPointcutAdvisor() {
+  //        final DefaultBeanFactoryPointcutAdvisor advisor = new DefaultBeanFactoryPointcutAdvisor();
+  //        advisor.setPointcut(new PublishResultMethodPointcut());
+  //        advisor.setAdviceBeanName(PUBLISH_RESULT_ADVICE_BEAN_NAME);
+  //        return advisor;
+  //    }
+  //
+  //    @Bean(name = PUBLISH_RESULT_ADVICE_BEAN_NAME)
+  //    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+  //    public PublishResultAfterReturnAdvice publishResultAfterReturnAdvice(ApplicationEventPublisher eventPublisher) {
+  //        return new PublishResultAfterReturnAdvice(eventPublisher);
+  //    }
 }
