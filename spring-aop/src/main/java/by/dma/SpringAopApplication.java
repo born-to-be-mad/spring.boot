f3ff5ff9ff669e79f1232a75bee358586e175e10
Dzmitry Marudau <vinmaster@tut.by>
2022-11-01 16:08:24 +0200

    Minor formatting

--- spring-aop/src/main/java/by/dma/SpringAopApplication.java
+++ spring-aop/src/main/java/by/dma/SpringAopApplication.java
@@ -1,47 +1,42 @@
 package by.dma;
 
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.locks.Lock;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import by.dma.aop.service.ExecutionServiceImpl;
 
 @SpringBootApplication
 public class SpringAopApplication {
 
-
-
    public static void main(String[] args) throws InterruptedException {
-    var ctx = new SpringApplicationBuilder()
-            .sources(SpringAopApplication.class)
-            //.web(WebApplicationType.NONE)
-            .web(WebApplicationType.SERVLET)
-            .run(args);
+       var ctx = new SpringApplicationBuilder()
+               .sources(SpringAopApplication.class)
+               //.web(WebApplicationType.NONE)
+               .web(WebApplicationType.SERVLET)
+               .run(args);
 
-    ExecutionServiceImpl executionService = ctx.getBean(ExecutionServiceImpl.class);
-    executionService.doBaseWork("MESSAGE");
-    executionService.doAdvancedWork("MESSAGE", "REQUIREMENTS");
-    executionService.processWithResult("MESSAGE_WITH_RESULT");
+       ExecutionServiceImpl executionService = ctx.getBean(ExecutionServiceImpl.class);
+       executionService.doBaseWork("MESSAGE");
+       executionService.doAdvancedWork("MESSAGE", "REQUIREMENTS");
+       executionService.processWithResult("MESSAGE_WITH_RESULT");
 
-    final Lock lock = ctx.getBean(Lock.class);
-    try {
-      lock.lock();
-      final Thread thread = new Thread(
-              () -> executionService.doExclusiveWork("MESSAGE"));
-      thread.start();
-      thread.join();
-    } finally {
-      lock.unlock();
-    }
+       final Lock lock = ctx.getBean(Lock.class);
+       try {
+           lock.lock();
+           final Thread thread = new Thread(
+                   () -> executionService.doExclusiveWork("MESSAGE"));
+           thread.start();
+           thread.join();
+       } finally {
+           lock.unlock();
+       }
   }
 
 /*
