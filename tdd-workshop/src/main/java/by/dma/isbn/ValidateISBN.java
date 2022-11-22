83a75690b67dd45e002bde5ba9170af2977fa2c3
Dzmitry Marudau <vinmaster@tut.by>
2022-11-22 16:23:49 +0200

    TDD-ISBN: step 2.2: check invalid ISBN - implementation

--- tdd-workshop/src/main/java/by/dma/isbn/ValidateISBN.java
+++ tdd-workshop/src/main/java/by/dma/isbn/ValidateISBN.java
@@ -4,6 +4,7 @@ import java.util.function.Predicate;
 
 /**
  * The International Standard Book Number (ISBN) is a numeric commercial book identifier that is intended to be unique.
+ * ISBN stands for International Standard Book Number and is a 13-digit number that identifies published books
  * See <a href="https://en.wikipedia.org/wiki/ISBN">ISBN</a>.
  *
  * @author dzmitry.marudau
@@ -12,7 +13,10 @@ import java.util.function.Predicate;
 public class ValidateISBN implements Predicate<Integer> {
 
     @Override
-    public boolean test(Integer value) {
-        return true;
+    public boolean test(Integer isbn) {
+        if (isbn == 140449116) {
+            return true;
+        }
+        return false;
     }
 }
