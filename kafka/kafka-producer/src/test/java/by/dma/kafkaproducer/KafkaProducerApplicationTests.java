226657b7a52e23e8cca6a42f2c9e5ea8c39884bb
Dzmitry Marudau <vinmaster@tut.by>
2022-11-23 18:08:22 +0200

    Spring Boot Apache Kafka: EmbeddedKafka in tests

--- kafka/kafka-producer/src/test/java/by/dma/kafkaproducer/KafkaProducerApplicationTests.java
+++ kafka/kafka-producer/src/test/java/by/dma/kafkaproducer/KafkaProducerApplicationTests.java
@@ -2,8 +2,13 @@ package by.dma.kafkaproducer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
 
+@EmbeddedKafka(bootstrapServersProperty = "spring.kafka.bootstrap-servers",
+topics = "pvtopic", brokerProperties = {"transaction.state.log.replication.factor=1"})
 @SpringBootTest
+@DirtiesContext
 class KafkaProducerApplicationTests {
 
     @Test
